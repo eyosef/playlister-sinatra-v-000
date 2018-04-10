02e0def81c0917adec024d6bf38a7d4e27dec1d4
Eden Mesfun <emesfun@gmail.com>
2018-04-10 15:06:15 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -50,7 +50,7 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    @genre = Genre.all
+    @genres = Genre.all
     erb :'songs/edit'
   end
 
